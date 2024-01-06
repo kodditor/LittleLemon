3166a07a8f2d60e0a3f1176168c01b3c784db9f5
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-01-06 23:59:16 +0000

    Created initial models

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class booking(models.Model):
+    id = models.IntegerField(primary_key=True,)
+    name = models.TextField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+class menu (models.Model):
+    id = models.IntegerField(primary_key = True)
+    title = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
